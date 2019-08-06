4364f8ba9d3084f42130dfd0299dfb9bb885602f
Shital <https://github.com/shital31/java-examples.git>
2019-08-06 15:27:04 +0530

    PrimeNumber

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -1,5 +1,22 @@
-public class PrimeNumber  {
-    public static void main(String args[]){
+import java.util.Scanner;
 
+public class PrimeNumber {
+    public static void main(String args[]) {
+        // int num ,i,j ;
+        //Scanner scanner  = new Scanner(System.in);
+        //System.out.print("Enter the value of number : ");
+        // num = scanner.nextInt();
+        for (int i = 2; i <= 100; i++) {
+            for (int j = 2; j <= i; j++) {
+                if (j == i) {
+                    System.out.println(i);
+                }
+                if (i % j == 0) {
+                    break;
+                }
+            }
+
+        }
     }
+
 }
