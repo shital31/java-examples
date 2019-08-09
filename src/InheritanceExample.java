cfe0e771505ad00091503d68a31ff66bd292f6f7
Shital <https://github.com/shital31/java-examples.git>
2019-08-09 17:00:04 +0530

    java examples

---
+++ src/InheritanceExample.java
@@ -0,0 +1,9 @@
+public class InheritanceExample {
+    void add(int a,int b){
+        System.out.println(" Result :"+(a+b));
+    }
+    public static void main(String args[]){
+        InheritanceExample add=new InheritanceExample();
+        add.add(10,20);
+    }
+}
---
+++ src/InheritanceExample1.java
@@ -0,0 +1,7 @@
+public class InheritanceExample1 extends InheritanceExample {
+    public static void main(String args[]){
+        InheritanceExample1 obj=new InheritanceExample1();
+        obj.add(10,20);
+    }
+
+}
