dbab39870e5a050b91cfbc47f2c5efc6288193a8
Shital <https://github.com/shital31/java-examples.git>
2019-12-06 07:57:13 +0530

    practice examples interview practice

---
+++ src/StringBufferReverse.java
@@ -0,0 +1,6 @@
+public class StringBufferReverse {
+    public static void main(String[] args) {
+        StringBuffer sbr = new StringBuffer("String");
+        System.out.println("reverse :"+ sbr.reverse());
+    }
+}
