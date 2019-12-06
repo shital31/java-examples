690352de08521f0b48dadd001e73f9ec4ad54e63
Shital <https://github.com/shital31/java-examples.git>
2019-12-06 07:57:52 +0530

    practice examples interview practice

---
+++ src/FibbonacciPractice.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FibbonacciPractice {
+    public static void main(String[] args) {
+        int a=0, b=1;
+        System.out.println(a +" "+b);
+        for(int i=1; i<=10; i++){
+            int c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+        }
+    }
+}
